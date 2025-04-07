492249ac790bc33ef0f38f2b6281b3bb73c05d20
15ho <fifteenho@163.com>
2025-04-07 23:48:04 +0800

    chore: add toys

---
+++ microservices/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // --- init
+    // server config
+    // tracing
+    // logging
+    // --- api protocol
+    // grpc
+    // http
+    // --- logic example(multiple services)
+    // aggregate service
+    // user service
+    // goods service
+    // payment service
+    // order service
+}
---
+++ restfulapi/src/main.rs
@@ -0,0 +1,24 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // TODO:
+    // --- init
+    // server config
+    // logging
+    // --- logic example
+    // user // user auth
+    // order // order status
+
+    HttpServer::new(|| {
+        App::new().service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
